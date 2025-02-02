b2fda51fbcf4c228cde4154ec507c4e8c1e73a42
18372738 <133884450+18372738@users.noreply.github.com>
2025-02-02 14:10:11 +0300

    Added file error_logging.pe

--- tg_bot.py
+++ tg_bot.py
@@ -6,18 +6,7 @@ from telegram import Update, ForceReply, Bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 from dialogflow import detect_intent_texts
-
-
-class TelegramLogsHandler(logging.Handler):
-
-    def __init__(self, bot, chat_id):
-        super().__init__()
-        self.chat_id = chat_id
-        self.bot = bot
-
-    def emit(self, record):
-        log_entry = self.format(record)
-        self.bot.send_message(chat_id=self.chat_id, text=log_entry)
+from error_logging import TelegramLogsHandler
 
 
 def start(update: Update, context: CallbackContext) -> None:
--- vk_bot.py
+++ vk_bot.py
@@ -8,18 +8,7 @@ from telegram import Update, ForceReply, Bot
 from environs import Env
 
 from dialogflow import detect_intent_texts
-
-
-class TelegramLogsHandler(logging.Handler):
-
-    def __init__(self, bot, chat_id):
-        super().__init__()
-        self.chat_id = chat_id
-        self.bot = bot
-
-    def emit(self, record):
-        log_entry = self.format(record)
-        self.bot.send_message(chat_id=self.chat_id, text=log_entry)
+from error_logging import TelegramLogsHandler
 
 
 def handle_vk_message(event, vk_client, project_id):
